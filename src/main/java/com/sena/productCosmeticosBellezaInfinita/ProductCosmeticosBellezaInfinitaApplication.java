8dc158c6bd00ea3dcb4a8520d8b118e2044943f5
breiner-r <breinerr006@gmail.com>
2025-04-18 15:59:09 -0500

    first commit

---
+++ src/main/java/com/sena/productCosmeticosBellezaInfinita/ProductCosmeticosBellezaInfinitaApplication.java
@@ -0,0 +1,13 @@
+package com.sena.productCosmeticosBellezaInfinita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCosmeticosBellezaInfinitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCosmeticosBellezaInfinitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sena/productCosmeticosBellezaInfinita/ProductCosmeticosBellezaInfinitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.productCosmeticosBellezaInfinita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCosmeticosBellezaInfinitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
